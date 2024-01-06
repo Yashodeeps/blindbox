0feac29c67126df2eee88bc945400d9cf8658bea
Yashodeep Nimbekar <117455443+Yashodeeps@users.noreply.github.com>
2024-01-07 00:01:13 +0530

    feat- login route

--- server/src/controllers/user.controller.js
+++ server/src/controllers/user.controller.js
@@ -3,47 +3,147 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { User } from "../models/user.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
+const generateAccessAndResopnseTokens = async (userId) => {
+    try {
+        const user = await User.findOne(userId);
+        const accessToken = await user.generateAccessToken();
+        const refreshToken = await user.generateRefreshToken();
+
+        user.refreshToken = refreshToken;
+        await user.save({ validateBeforeSave: false });
+
+        return { accessToken, refreshToken };
+    } catch (error) {
+        throw new ApiError(
+            500,
+            "Something went wrong while generating refresh token and access token"
+        );
+    }
+};
+
 const registerUser = asyncHandler(async (req, res) => {
-  //get user details
+    //get user details
 
-  const { username, email, password, nickName } = req.body;
-  console.log(username);
+    const { username, email, password, nickName } = req.body;
+    console.log(username);
 
-  if (
-    [username, email, password, nickName].some((field) => field?.trim() === "")
-  ) {
-    throw new ApiError(400, "All fields are required");
-  }
+    if (
+        [username, email, password, nickName].some(
+            (field) => field?.trim() === ""
+        )
+    ) {
+        throw new ApiError(400, "All fields are required");
+    }
 
-  //check whether whether user exists or not
-  const existedUser = await User.findOne({
-    $or: [{ username }, { email }],
-  });
+    //check whether whether user exists or not
+    const existedUser = await User.findOne({
+        $or: [{ username }, { email }],
+    });
 
-  if (existedUser) {
-    throw new ApiError(409, "username or email already exists");
-  }
+    if (existedUser) {
+        throw new ApiError(409, "username or email already exists");
+    }
 
-  const user = await User.create({
-    nickName,
-    email,
-    password,
-    username: username.toLowerCase(),
-  });
+    const user = await User.create({
+        nickName,
+        email,
+        password,
+        username: username.toLowerCase(),
+    });
 
-  const createdUser = await User.findById(user._id).select(
-    "-password -refreshToken"
-  ); //in .select method, we write the things we dont want using a - sign and a space in a string
+    const createdUser = await User.findById(user._id).select(
+        "-password -refreshToken"
+    ); //in .select method, we write the things we dont want using a - sign and a space in a string
 
-  if (!createdUser) {
-    throw new ApiError(500, "Something went wrong while creating a user");
-  }
+    if (!createdUser) {
+        throw new ApiError(500, "Something went wrong while creating a user");
+    }
 
-  return res
-    .status(201)
-    .json(new ApiResponse(200, "USer registered successfully"));
+    return res
+        .status(201)
+        .json(new ApiResponse(200, "USer registered successfully"));
+});
+
+const loginUser = asyncHandler(async (req, res) => {
+    const { username, email, password } = req.body;
+
+    if (!username && !email) {
+        throw new ApiError(400, "email or username is required");
+    }
+
+    const user = await User.findOne({
+        $or: [{ username }, { email }],
+    });
+
+    if (!user) {
+        throw new ApiError(404, "user not found");
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password);
+
+    if (!isPasswordValid) {
+        throw new ApiError(401, "Invalid user credentials");
+    }
+
+    const { accessToken, refreshToken } = await generateAccessAndResopnseTokens(
+        user._id
+    );
+
+    const loggedInUSer = await User.findById(user._id).select(
+        "-password -refreshToken"
+    );
+
+    //this options doesnt allow to modify cookies from frontend,
+    //can only be modifyable from server
+    const options = {
+        httpOnly: true,
+        secure: true,
+    };
+
+    return res
+        .status(200)
+        .cookie("accessToken", accessToken, options)
+        .cookie("refreshToken", refreshToken, options)
+        .json(
+            new ApiResponse(
+                200,
+                {
+                    user: loggedInUSer, //if user want to store tokens from his side or develop mobile apps(no cookies in mobiles  )
+                    accessToken,
+                    refreshToken,
+                },
+                "User logged In successfully"
+            )
+        );
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+    //remove refresh token from db
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined,
+            },
+        },
+        {
+            new: true,
+        }
+    );
+
+    const options = {
+        httpOnly: true,
+        secure: true,
+    };
+
+    //delete cookies
+    return res
+        .status(200)
+        .clearCookie("accessToken", options)
+        .clearCookie("refreshToken", options)
+        .json(new ApiResponse(200, {}, "User logged out"));
 });
 
 //use Zod for valadation
 
-export { registerUser };
+export { registerUser, loginUser, logoutUser };
